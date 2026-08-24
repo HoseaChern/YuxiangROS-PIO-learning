1d737bda54df6b1dd69070e862ce91dd8f356902
HoseaChern <chx_050913@163.com>
2026-08-24 15:11:35 +0800

    style(kinematics): 代码格式化

--- lib/Kinematics/Kinematics.cpp
+++ lib/Kinematics/Kinematics.cpp
@@ -83,7 +83,9 @@ void Kinematics::update_motor_speed(uint64_t now, const int32_t ticks[2]) {
     update_odom(dt);
 }
 
-float Kinematics::get_motor_speed(uint8_t motor_id) const { return current_motor_speeds_[motor_id]; }
+float Kinematics::get_motor_speed(uint8_t motor_id) const {
+    return current_motor_speeds_[motor_id];
+}
 
 void Kinematics::update_odom(uint64_t dt) {
     // 单位换算, ms -> s
